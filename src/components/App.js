6dab29c2f5b4523971186d864b839501b1f672c1
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-05-23 00:05:43 +0200

    Creating CoursePage class, creating a courseAction and the reducer

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import NotFoundPage from "./NotFoundPage";
+import CoursesPage from "./CoursesPage";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -9,6 +9,10 @@ const Head = () => {
         Home
       </NavLink>
       {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses
+      </NavLink>
+      {" | "}
       <NavLink to="/about" activeStyle={activeStyle}>
         About
       </NavLink>
