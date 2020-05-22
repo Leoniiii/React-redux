b1b186c0f4061769b3108d7ee2d9ff6a9c2db04b
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-05-22 11:29:50 +0200

    Creating header and not found page

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const NotFoundPage = () => <h1>Oops! Not found page!</h1>;
+
+export default NotFoundPage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Head = () => {
+  const activeStyle = { color: "#f15b2a" };
+  return (
+    <nav>
+      <NavLink to="/" exact activeStyle={activeStyle}>
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Head;
